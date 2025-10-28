71e9caac4d6729410644ddb276c760abb0d9a076
Borja-68 <borjapazpar@gmail.com>
2025-10-28 17:14:21 +0100

    creada clase para parametros

--- TriviaApp/app/src/main/java/com/example/triviaapp/MainActivity.kt
+++ TriviaApp/app/src/main/java/com/example/triviaapp/MainActivity.kt
@@ -11,8 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.triviaapp.ui.theme.TriviaAppTheme
-
+import com.example.compose.TriviaAppTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- TriviaApp/app/src/main/java/com/example/triviaapp/componentes/ComponenteTarjetaHorizontal.kt
+++ TriviaApp/app/src/main/java/com/example/triviaapp/componentes/ComponenteTarjetaHorizontal.kt
@@ -6,7 +6,9 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -14,23 +16,35 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.triviaapp.R
-
+class CartaInfo(var accion: () -> Unit={},var imagen : Int, var nombre : String)
 @Composable
-fun ComponenteTarjetaHorizontal(imagen: Int,TxtMsg: String,accion:()-> Unit){
-
-    Row(modifier = Modifier.clickable(onClick = accion).background(color = Color.White).padding(horizontal = 2.dp, vertical = 2.dp)) {
-        Image(painter = painterResource(id = imagen), modifier = Modifier.size(width = 20.dp, height = 20.dp),contentDescription = "imagen categoria")
-        Text(text = TxtMsg , modifier = Modifier.padding(horizontal = 6.dp))
+fun ComponenteTarjetaHorizontal(infoCarta : CartaInfo) {
+    Row(
+        modifier = Modifier
+            .clickable(onClick = infoCarta.accion)
+            .background(color = MaterialTheme.colorScheme.background)
+            .padding(horizontal = 2.dp, vertical = 2.dp)
+    ) {
+        Image(
+            painter = painterResource(id = infoCarta.imagen),
+            modifier = Modifier.size(width = 20.dp, height = 20.dp),
+            contentDescription = "imagen categoria"
+        )
+        Text(text = infoCarta.nombre,
+            modifier = Modifier.padding(horizontal = 6.dp),
+        )
     }
 
 }
 
 @Preview
 @Composable
-fun PrevioTarjHoriz(){
-    val imagen= R.drawable.ic_launcher_background
-    ComponenteTarjetaHorizontal(imagen,"abuela",{})
+fun PrevioTarjHoriz() {
+    val imagen = R.drawable.ic_launcher_background
+    var infoCarta= CartaInfo(imagen = R.drawable.ic_launcher_background, nombre = "abuela")
+    ComponenteTarjetaHorizontal(infoCarta)
 }
