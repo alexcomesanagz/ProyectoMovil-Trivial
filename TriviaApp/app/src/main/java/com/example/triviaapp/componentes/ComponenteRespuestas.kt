ffde4dcff57bd49fd4027eaca22661f42c115765
a24alexcg <alexcomesanagz@gmail.com>
2025-10-30 19:16:59 +0100

    ComponenteRespuestasFinal

---
+++ TriviaApp/app/src/main/java/com/example/triviaapp/componentes/ComponenteRespuestas.kt
@@ -0,0 +1,42 @@
+package com.example.triviaapp.componentes
+
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ComponenteRespuestas(botonesRespuesta: List<String>) {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        content = {
+            items(botonesRespuesta) { textoBoton ->
+                Box(
+                    contentAlignment = Alignment.Center
+                ) {
+                    AceptartBoton(textoBoton)
+                }
+            }
+        }
+    )
+}
+
+
+@Preview(showSystemUi = true)
+@Composable
+fun prevComponenteRespuestas() {
+    val botones = listOf(
+        "Respuesta 1",
+        "Respuesta 2",
+        "Respuesta 3",
+        "Respuesta 4"
+    )
+    ComponenteRespuestas(botones)
+}
