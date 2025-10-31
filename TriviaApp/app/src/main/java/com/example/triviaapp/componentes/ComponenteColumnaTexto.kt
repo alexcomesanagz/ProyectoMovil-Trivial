036f23c4e7c5a330a5fcb2d316cb6adcd8403b9f
elgoc <Borjapazpar@gmail.com>
2025-10-31 18:37:47 +0100

    segunda funcion para Columna texto

--- TriviaApp/app/src/main/java/com/example/triviaapp/componentes/ComponenteColumnaTexto.kt
+++ TriviaApp/app/src/main/java/com/example/triviaapp/componentes/ComponenteColumnaTexto.kt
@@ -1,17 +1,22 @@
 package com.example.triviaapp.componentes
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 
 @Composable
-fun ComponenteColumnaTexto(msj1: String,msj2: String){
+fun ComponenteColumnaTextoDobleFondo(msj1: String,msj2: String){
     Column(verticalArrangement = Arrangement.spacedBy(5.dp),
         modifier = Modifier.fillMaxWidth()
             .padding(vertical = 4.dp, horizontal = 100.dp)) {
@@ -19,8 +24,25 @@ fun ComponenteColumnaTexto(msj1: String,msj2: String){
         ComponenteTitulo(titulo = msj2)
     }
 }
+
+@Composable
+fun ComponenteColumnaTextoUnFondo(msj1: String,msj2: String){
+    Column(verticalArrangement = Arrangement.spacedBy(5.dp),
+        modifier = Modifier.fillMaxWidth()
+            .padding(vertical = 4.dp, horizontal = 100.dp)) {
+        ComponenteTitulo(titulo = msj1)
+        Text(text = msj2, modifier = Modifier.padding(vertical = 4.dp, horizontal = 20.dp)
+            .fillMaxWidth(),
+            textAlign = TextAlign.Center)
+    }
+}
+
+
 @Preview (showSystemUi = true)
 @Composable
 fun PreviewColumnTexto(){
-ComponenteColumnaTexto("si","no")
+    Column {
+ComponenteColumnaTextoDobleFondo("si","no")
+        ComponenteColumnaTextoUnFondo("si","no")
+    }
 }
