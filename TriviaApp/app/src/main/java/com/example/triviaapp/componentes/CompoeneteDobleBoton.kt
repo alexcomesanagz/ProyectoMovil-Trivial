e056043e3a42cf84763bffade1905f024699c766
Borja-68 <borjapazpar@gmail.com>
2025-11-04 17:11:48 +0100

    pagina lista bonita a medias

--- TriviaApp/app/src/main/java/com/example/triviaapp/componentes/CompoeneteDobleBoton.kt
+++ TriviaApp/app/src/main/java/com/example/triviaapp/componentes/CompoeneteDobleBoton.kt
@@ -47,7 +47,7 @@ fun BotonesDobleAceptarLinea(msjBot1: String, msjBot2: String) {
 
 @Composable
 fun BotonesDobleAceptarColumna(msjBot1: String, msjBot2: String) {
-    Column(
+    Column(horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
             .padding(vertical = 5.dp)
             .fillMaxWidth(),
