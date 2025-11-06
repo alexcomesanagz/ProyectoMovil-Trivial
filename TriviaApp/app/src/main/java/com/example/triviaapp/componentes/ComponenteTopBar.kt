744b959fe6e43296a4a7be9ce3b2b3b289b08ed2
Borja-68 <borjapazpar@gmail.com>
2025-11-06 18:41:36 +0100

    cambio al nombre de componente top bar y añadidos logs a sus botones

--- TriviaApp/app/src/main/java/com/example/triviaapp/componentes/ComponenteTopBar.kt
+++ TriviaApp/app/src/main/java/com/example/triviaapp/componentes/ComponenteTopBar.kt
@@ -1,30 +1,27 @@
 package com.example.triviaapp.componentes
 
+import android.util.Log
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Create
 import androidx.compose.material.icons.filled.ExitToApp
 import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.icons.filled.MoreVert
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TopBarComponent(
+fun ComponenteTopBar(
     title: String,
     onMenuClick: () -> Unit={},
-    onSearchClick: () -> Unit={},
-    onMoreClick: () -> Unit={}
+    onSearchClick: () -> Unit={ Log.e("Testeo","camara cliqueada")},
+    onMoreClick: () -> Unit={Log.e("Testeo","perfil cliqueada")}
 ) {
     TopAppBar(
         title = { Text(text = title, textAlign = TextAlign.Center) },
@@ -50,5 +47,5 @@ fun TopBarComponent(
 @Preview
 @Composable
 fun PreviewTop(){
-    TopBarComponent("awa",{},{},{})
+    ComponenteTopBar("awa",{},{},{})
 }
