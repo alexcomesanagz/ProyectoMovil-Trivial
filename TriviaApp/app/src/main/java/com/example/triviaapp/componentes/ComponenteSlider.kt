4ed16bf6c5261f0f4c940cbb92ee2c9689214aaa
Alex <alexcomesanagz@gmail.com>
2025-11-07 23:38:56 +0100

    commitSliderFinal

---
+++ TriviaApp/app/src/main/java/com/example/triviaapp/componentes/ComponenteSlider.kt
@@ -0,0 +1,48 @@
+package com.example.triviaapp.componentes
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Slider
+import androidx.compose.material3.SliderDefaults
+import androidx.compose.material3.Switch
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+/**
+ * barra con número del 1 al 20
+ * llendo de 1 en 1
+ * número 10 por defecto
+ */
+@Composable
+fun ComponenteSlider(){
+    var sliderPosition by remember { mutableStateOf(10f) }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Text(text= "Valor: " + sliderPosition)
+        Slider(
+            value = sliderPosition,
+            onValueChange = { sliderPosition = it },
+            valueRange = 1f..20f,
+            steps = 18,
+        )
+    }
+}
+
+@Preview
+@Composable
+fun PrevComponenteSlider(){
+    ComponenteSlider()
+}
