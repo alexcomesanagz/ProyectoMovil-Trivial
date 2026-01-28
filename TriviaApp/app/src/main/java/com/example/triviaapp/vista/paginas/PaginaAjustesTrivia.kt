4ab70135989760c149babf27270c53fb8d60ec08
elgoc <Borjapazpar@gmail.com>
2026-01-28 11:49:54 +0100

    viewmodel en pagina ajustes

--- TriviaApp/app/src/main/java/com/example/triviaapp/vista/paginas/PaginaAjustesTrivia.kt
+++ TriviaApp/app/src/main/java/com/example/triviaapp/vista/paginas/PaginaAjustesTrivia.kt
@@ -5,23 +5,28 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.triviaapp.R
 import com.example.triviaapp.componentes.BotonesAceptarDenegarLinea
 import com.example.triviaapp.componentes.ComponenteCajaDatosTrivia
 import com.example.triviaapp.componentes.ComponenteLinea
 import com.example.triviaapp.componentes.DatosBotonDoble
+import com.example.triviaapp.viewModels.PaginaFinViewModel
+import com.example.triviaapp.viewModels.Uis.PaginaAjustesUi
 
 /**
  * Pagina para introducir datos del trivia a crear
  */
 
 @Composable
-fun PaginaAjustesTrivia(
-) {
+fun PaginaAjustesTrivia(paginaAjustesUi: PaginaFinViewModel= viewModel()) {
+    val uiState by paginaAjustesUi.uiState.collectAsState()
     Column(
         modifier = Modifier.padding(vertical = 15.dp, horizontal = 15.dp),
         verticalArrangement = Arrangement.spacedBy(40.dp)
