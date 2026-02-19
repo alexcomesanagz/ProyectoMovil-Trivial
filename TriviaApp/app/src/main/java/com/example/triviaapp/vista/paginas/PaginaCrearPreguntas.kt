c93b8c76e76419573851193767b2f9ff16e8edd6
elgoc <Borjapazpar@gmail.com>
2026-02-19 14:20:58 +0100

    lazy column crear

--- TriviaApp/app/src/main/java/com/example/triviaapp/vista/paginas/PaginaCrearPreguntas.kt
+++ TriviaApp/app/src/main/java/com/example/triviaapp/vista/paginas/PaginaCrearPreguntas.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
@@ -52,75 +53,78 @@ fun PaginaElegirRespuestas(idTrivia:String, crearPreguntas: CrearRespViewModel =
     if (preguntaActual == null) {
         return
     }
-
-    Box(
-        Modifier
-            .fillMaxSize()
-            .padding(vertical = 30.dp, horizontal = 10.dp)
-            .windowInsetsPadding(WindowInsets.safeDrawing)
-    ) {
-        Column(
-            verticalArrangement = Arrangement.spacedBy(30.dp)
-
-        ) {
-            Column(verticalArrangement = Arrangement.spacedBy(30.dp)) {
-                ComponentePreguntaYRespuestasRellenar(
-                    DatosCreaPregunta(
-                        enunciado = preguntaActual.pregunta,
-                        textoBotonesRespuesta = preguntaActual.textoBotonesRespuestas,
-                        accionEnunciado = {it-> crearPreguntas.cambiaPregunta(it)},
-                        accionRespuestas = {i,it->crearPreguntas.cambiaTextoBoton(i,it)}
-                    )
-                )
-                ComponenteTituloConRadioButonHorizontal(
-                    stringResource(R.string.app_opcion_correcta),
-                    txtBotones,
-                    remember = preguntaActual.respuestaCorrecta,
-                    accion = {it-> crearPreguntas.cambiaRespuestaCorrecta(it)}
-                )
-            }
-            Column(
-                verticalArrangement = Arrangement.spacedBy(10.dp),
-                modifier = Modifier
-                    .padding(bottom = 10.dp)
-                    .background(
-                        MaterialTheme.colorScheme.surface,
-                        RoundedCornerShape(12.dp)
-                    )
+    LazyColumn {
+        item {
+            Box(
+                Modifier
+                    .fillMaxSize()
+                    .padding(vertical = 30.dp, horizontal = 10.dp)
+                    .windowInsetsPadding(WindowInsets.safeDrawing)
             ) {
-                ComponenteTituloCaja( "${uiState.i+1} / ${crearPreguntas.getNumPreguntas()} ")
-                Box(
-                    modifier = Modifier.padding(bottom = 10.dp)
+                Column(
+                    verticalArrangement = Arrangement.spacedBy(30.dp)
+
                 ) {
-                    BotonesDobleAvanzarLinea(
-                        DatosBotonDoble(
-                            stringResource(R.string.app_bt_anterior),
-                            stringResource(R.string.app_bt_siguiente),
-                            accionBoton1 = { crearPreguntas.anteriorPregunta() },
-                            accionBoton2 = { crearPreguntas.siguientePregunta()})
-                    )
-                }
-            }
-            Column(
-                verticalArrangement = Arrangement.spacedBy(10.dp)
-            ) {
-                ComponenteLinea()
-                BotonesAceptarDenegarLinea(
-                    DatosBotonDoble(
-                        stringResource(R.string.app_bt_salir),
-                        stringResource(R.string.app_bt_finalizar),
-                        accionBoton1 = {
-                            crearPreguntas.cancelarCreacion(
-                            idTrivia = idTrivia,
-                            onSucces = onClickSalir,
-                            onError = {}
+                    Column(verticalArrangement = Arrangement.spacedBy(30.dp)) {
+                        ComponentePreguntaYRespuestasRellenar(
+                            DatosCreaPregunta(
+                                enunciado = preguntaActual.pregunta,
+                                textoBotonesRespuesta = preguntaActual.textoBotonesRespuestas,
+                                accionEnunciado = {it-> crearPreguntas.cambiaPregunta(it)},
+                                accionRespuestas = {i,it->crearPreguntas.cambiaTextoBoton(i,it)}
+                            )
                         )
-                        },
-                        accionBoton2 = {
-                            crearPreguntas.fin(onClickAceptar,{})
+                        ComponenteTituloConRadioButonHorizontal(
+                            stringResource(R.string.app_opcion_correcta),
+                            txtBotones,
+                            remember = preguntaActual.respuestaCorrecta,
+                            accion = {it-> crearPreguntas.cambiaRespuestaCorrecta(it)}
+                        )
+                    }
+                    Column(
+                        verticalArrangement = Arrangement.spacedBy(10.dp),
+                        modifier = Modifier
+                            .padding(bottom = 10.dp)
+                            .background(
+                                MaterialTheme.colorScheme.surface,
+                                RoundedCornerShape(12.dp)
+                            )
+                    ) {
+                        ComponenteTituloCaja( "${uiState.i+1} / ${crearPreguntas.getNumPreguntas()} ")
+                        Box(
+                            modifier = Modifier.padding(bottom = 10.dp)
+                        ) {
+                            BotonesDobleAvanzarLinea(
+                                DatosBotonDoble(
+                                    stringResource(R.string.app_bt_anterior),
+                                    stringResource(R.string.app_bt_siguiente),
+                                    accionBoton1 = { crearPreguntas.anteriorPregunta() },
+                                    accionBoton2 = { crearPreguntas.siguientePregunta()})
+                            )
                         }
-                    )
-                )
+                    }
+                    Column(
+                        verticalArrangement = Arrangement.spacedBy(10.dp)
+                    ) {
+                        ComponenteLinea()
+                        BotonesAceptarDenegarLinea(
+                            DatosBotonDoble(
+                                stringResource(R.string.app_bt_salir),
+                                stringResource(R.string.app_bt_finalizar),
+                                accionBoton1 = {
+                                    crearPreguntas.cancelarCreacion(
+                                    idTrivia = idTrivia,
+                                    onSucces = onClickSalir,
+                                    onError = {}
+                                )
+                                },
+                                accionBoton2 = {
+                                    crearPreguntas.fin(onClickAceptar,{})
+                                }
+                            )
+                        )
+                    }
+                }
             }
         }
     }
